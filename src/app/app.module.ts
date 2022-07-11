0435e8f2d72b922793497e7807118b2aafe78eb2
RubenHoodESP <RubenHoodSpain@gmail.com>
2022-07-11 23:20:56 +0200

    animaciones, logo y botón añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './screens/start/start.component';
 import { CombatComponent } from './screens/combat/combat.component';
 import { SoundsComponent } from './components/sounds/sounds.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
     CombatComponent,
-    SoundsComponent
+    SoundsComponent,
+    OpenCloseComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/screens/start/start.component.ts
+++ src/app/screens/start/start.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-start',
+  animations: [
+    trigger('appear', [
+      state('on', style({
+        width: '60%',
+        opacity: 1
+      })),
+      state('off', style({
+        width: '50%',
+        opacity: 0
+      })),
+      transition('on => off', [
+        animate('1.5s')
+      ]),
+      transition('off => on', [
+        animate('1.5s')
+      ]),
+    ]),
+    trigger('display', [
+      state('yes', style({
+        display: 'block',
+        opacity: '1'
+      })),
+      state('no', style({
+        display: 'none',
+        opacity: '0'
+      })),
+      transition('yes => no', [
+        animate('1.5s')
+      ]),
+      transition('no => yes', [
+        animate('1.5s')
+      ]),
+    ])
+  ],
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
 
+  isOn = false;
+  isDisplayed = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  toggle() {
+    this.isOn = !this.isOn;
+    this.isDisplayed = !this.isDisplayed;
   }
 
 }
